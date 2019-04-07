1dc418e156188b0e7ddae6817c4d47eb51bcd984
hpark95 <parkhs1995@gmail.com>
2019-04-07 13:01:33 -0400

    mu.py updated

--- mu.py
+++ mu.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+
+
 def mu(A, k, num_iter):
 	'''
 	Run multiplicative updates to perform nonnegative matrix factorization on A.
@@ -20,12 +22,42 @@ def mu(A, k, num_iter):
 		H: ndarray
 			- k by n matrix where k = dim
 	'''
-	W = np.zeros(np.size(A, 0), k)
-	H = np.zeros(k, np.size(A, 1))
+
+	print('Applying multiplicative updates on the input matrix...')
+	print('---------------------------------------------------------------------')
+
+	V = A
+	W = np.ones((np.size(A, 0), k))
+	H = np.ones((k, np.size(A, 1)))
 
 	for n in range(num_iter):
-		for i in range(np.size())
-			
+		# Update W
+		VH_T = V @ H.T
+		WHH_T =  W @ H @ H.T
 
+		for i in range(np.size(W, 0)):
+			for j in range(np.size(W, 1)):
+				W[i, j] = W[i, j] * VH_T[i, j] / WHH_T[i, j]
+
+		# Update H
+		W_TV = W.T @ V
+		W_TWH = W.T @ W @ H
+
+		for i in range(np.size(H, 0)):
+			for j in range(np.size(H, 1)):
+				H[i, j] = H[i, j] * W_TV[i, j] / W_TWH[i, j]
+
+	print('A = ')
+	print(A)
+	print('W = ')
+	print(W)
+	print('H = ')
+	print(H)
+	print('W * H = ')
+	print(W @ H)
 
 	return W, H
+
+if __name__ == '__main__':
+	A = np.matrix([[1, 2], [3, 4]])
+	W, H = mu(A, 3, 100)

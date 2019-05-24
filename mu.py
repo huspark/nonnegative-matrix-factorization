8fab7a8d4e456c637c6f0080563573a31781a336
hpark95 <parkhs1995@gmail.com>
2019-05-23 22:24:31 -0400

    readability improved

--- mu.py
+++ mu.py
@@ -1,80 +1,75 @@
 import numpy as np
 
 
-def mu(A, k, delta, num_iter, init_W = None, init_H = None, print_enabled = False):
-	'''
-	Run multiplicative updates to perform nonnegative matrix factorization on A.
-	Return matrices W, H such that A = WH.
-	
-	Parameters:
-		A: ndarray
-			- m by n matrix to factorize
-		k: int
-			- integer specifying the column length of W / the row length of H
-			- the resulting matrices W, H will have sizes of m by k and k by n, respectively
-		delta: float
-			- float that will be added to the numerators of the update rules
-			- necessary to avoid division by zero problems
-		num_iter: int
-			- number of iterations for the multiplicative updates algorithm
-		init_W: ndarray
-			- m by k matrix for the initial W
-		init_H: ndarray
-			- k by n matrix for the initial H
-		print_enabled: boolean
-			- if ture, output print statements
+def mu(A, k, delta, num_iter, init_W=None, init_H=None, print_enabled=False):
+    '''
+    Run multiplicative updates to perform nonnegative matrix factorization on A.
+    Return matrices W, H such that A = WH.
 
-	Returns:
-		W: ndarray
-			- m by k matrix where k = dim
-		H: ndarray
-			- k by n matrix where k = dim
-	'''
+    Parameters:
+        A: ndarray
+            - m by n matrix to factorize
+        k: int
+            - integer specifying the column length of W / the row length of H
+            - the resulting matrices W, H will have sizes of m by k and k by n, respectively
+        delta: float
+            - float that will be added to the numerators of the update rules
+            - necessary to avoid division by zero problems
+        num_iter: int
+            - number of iterations for the multiplicative updates algorithm
+        init_W: ndarray
+            - m by k matrix for the initial W
+        init_H: ndarray
+            - k by n matrix for the initial H
+        print_enabled: boolean
+            - if ture, output print statements
 
-	print('Applying multiplicative updates on the input matrix...')
+    Returns:
+        W: ndarray
+            - m by k matrix where k = dim
+        H: ndarray
+            - k by n matrix where k = dim
+    '''
 
-	if print_enabled:
-		print('---------------------------------------------------------------------')
-		print('Frobenius norm ||A - WH||_F')
-		print('')
+    print('Applying multiplicative updates on the input matrix...')
 
-	V = A
+    if print_enabled:
+        print('---------------------------------------------------------------------')
+        print('Frobenius norm ||A - WH||_F')
+        print('')
 
-	# Initialize W and H
-	if init_W is None:
-		W = np.random.rand(np.size(A, 0), k)
-	else:
-		W = init_W
+    # Initialize W and H
+    if init_W is None:
+        W = np.random.rand(np.size(A, 0), k)
+    else:
+        W = init_W
 
-	if init_H is None:
-		H = np.random.rand(k, np.size(A, 1))
-	else:
-		H = init_H
+    if init_H is None:
+        H = np.random.rand(k, np.size(A, 1))
+    else:
+        H = init_H
 
-	# Decompose the input matrix
-	for n in range(num_iter):
+    # Decompose the input matrix
+    for n in range(num_iter):
 
-		# Update H
-		W_TA = W.T @ A
-		W_TWH = W.T @ W @ H + delta
+        # Update H
+        W_TA = W.T @ A
+        W_TWH = W.T @ W @ H + delta
 
-		for i in range(np.size(H, 0)):
-			for j in range(np.size(H, 1)):
-				H[i, j] = H[i, j] * W_TA[i, j] / W_TWH[i, j]
+        for i in range(np.size(H, 0)):
+            for j in range(np.size(H, 1)):
+                H[i, j] = H[i, j] * W_TA[i, j] / W_TWH[i, j]
 
-		# Update W
-		AH_T = A @ H.T
-		WHH_T =  W @ H @ H.T + delta
+        # Update W
+        AH_T = A @ H.T
+        WHH_T =  W @ H @ H.T + delta
 
-		for i in range(np.size(W, 0)):
-			for j in range(np.size(W, 1)):
-				W[i, j] = W[i, j] * AH_T[i, j] / WHH_T[i, j]
+        for i in range(np.size(W, 0)):
+            for j in range(np.size(W, 1)):
+                W[i, j] = W[i, j] * AH_T[i, j] / WHH_T[i, j]
 
-		if print_enabled:
-			frob_norm = np.linalg.norm(A - W @ H, 'fro')
-			print("iteration " + str(n + 1) + ": " + str(frob_norm))
+        if print_enabled:
+            frob_norm = np.linalg.norm(A - W @ H, 'fro')
+            print("iteration " + str(n + 1) + ": " + str(frob_norm))
 
-	return W, H
-
-
-if __name__ == '__main__':
+    return W, H
